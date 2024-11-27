1aee3fd9770698c215c9d9150e74e173422897b2
Deyvid Vasilev <deyvidapps@gmail.com>
2024-11-27 16:34:41 +0000

    Creating Header and Background color

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,28 @@
 </script>
 
 <template>
-  <div class="text-2xl font-bold text-blue-500 underline">
-    Test
+
+  <div class="bg-[#F5F5F5] min-h-screen">
+    <header class="bg-[#4d4d4d] text-white px-4 py-3 flex items-center justify-between">
+      <!-- Title -->
+      <div class="text-xl font-bold">
+        ActivityHive
+      </div>
+
+      <!-- Buttons -->
+      <div class="flex justify-center w-full space-x-4">
+        <button class="bg-white text-[#4d4d4d] px-4 py-2 rounded shadow hover:bg-gray-100 transition">
+          Shop
+        </button>
+        <button class="bg-white text-[#4d4d4d] px-4 py-2 rounded shadow hover:bg-gray-100 transition">
+          Cart
+        </button>
+      </div>
+    </header>
+
+
   </div>
+
 </template>
 
 <style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customGray: '#F5F5F5',
+      },
+    },
   },
   plugins: [],
 }
